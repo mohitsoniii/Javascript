faf2b6b8e33299407058065975cded3b406ff2d2
Mohit Soni <131435634+mohitsoniii@users.noreply.github.com>
2024-07-16 14:14:25 +0000

    Comaparision of  Datatpes

---
+++ 01_Basics/04_comparisionOfDatatypes.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2<1);
+// console.log(2==1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null ==  0);
+// console.log(null >= 0);  //avoid these type of comparisions
+// console.log(null <= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);//avoid these type of comparision
+
+
+
+// ===  (strict check)=> (check's both value and datatypes)
+
+console.log("2" === 2);
