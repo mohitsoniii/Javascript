48addcef67f1f2932260b4facc45f875d322e437
mohitsoniii <2021pcecrmohit010@poornima.org>
2024-07-30 17:27:39 +0530

    Summary of Memories

--- Stack_and_Heap_Memory.js
+++ Stack_and_Heap_Memory.js
@@ -18,3 +18,26 @@ console.log(anotherName);  // output : stopped_being_mohit
  * is passed into B, so when we try to update the variable B then the value of A is not gets updated because the value of the copy of A is
  * passed into B inspite of refrance, so the original Value of A is Unaffected. 
  */
+
+
+
+
+//                        Heap Memory (Non-Primitive)
+
+let studOne = {
+    email : "user@gmail.com",
+    roll : 010 
+}
+
+let studTwo = studOne
+
+studTwo.email = "mohit@gmail.com"
+
+console.log(studOne.email);   // output : mohit@gmail.com
+console.log(studTwo.email);   // output : mohit@gmail.com
+
+/**
+ * In Heap Memory when a Variable A is passed in variable B , the actaually it's refrance is passed into the another variable i.e variable B,
+ * and when we try to update the variable B the it is updated in the memory where th refrance is and also the value at that refrance is also 
+ * gets updated, and then the value of variable A is also gets Upadtaed.
+ */
