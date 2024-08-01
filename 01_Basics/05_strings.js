0537c27003318037fa4e7206da12fe0fbe73e876
mohitsoniii <2021pcecrmohit010@poornima.org>
2024-08-01 18:23:45 +0530

    Learning Strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -17,8 +17,32 @@ const age = 24
 // another way of declaring the strings
 const workoutName = new String("Bench-Press")
 
-console.log(workoutName);
-console.log(workoutName.length);
-console.log(workoutName.charAt(6));
-console.log(workoutName.indexOf('P'));
-console.log(workoutName.toUpperCase());
+// console.log(workoutName[2]);
+// console.log(workoutName.__proto__);
+// console.log(workoutName.__proto__.length);
+// console.log(workoutName.length);
+// console.log(workoutName.charAt(6));
+// console.log(workoutName.indexOf('P'));
+// console.log(workoutName.toUpperCase());
+
+// *********   SUBSTRING   **********//
+// const newStr = workoutName.substring(0,4)
+// console.log(newStr);
+
+
+//***********   SLICE   ***********//
+// const anotherStr = workoutName.slice(-4,-1)
+// console.log(anotherStr);
+
+
+
+//*********     TRIM     ***********//
+// const str1 = "      mohit      "
+// console.log(str1);
+// console.log(str1.trim());
+
+
+
+// ***********    REPLACE     **********//
+// const url = "https://www.geeksforgeeks.org/user%50/mohitsoni18/"
+// console.log(url.replace('%50', '#'))
