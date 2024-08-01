77900c2a2c226ca1185b04e6526311591c5c3e80
mohitsoniii <2021pcecrmohit010@poornima.org>
2024-08-01 17:55:27 +0530

    Learning Strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,24 @@
+//          STRING            //
+
+const name = "Mohit Soni"
+const age = 24
+
+// console.log(name + age + "down");     // outdated style
+// console.log(typeof name);
+
+
+
+// *******STRING INTERPOLATION************//
+
+
+// console.log(`My name is ${name}. My string is ${age}`);  // (styring interpolation)
+
+
+// another way of declaring the strings
+const workoutName = new String("Bench-Press")
+
+console.log(workoutName);
+console.log(workoutName.length);
+console.log(workoutName.charAt(6));
+console.log(workoutName.indexOf('P'));
+console.log(workoutName.toUpperCase());
