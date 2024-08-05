c92ecefedeb6eb9f67590d35c4e0b3a617b6b43f
mohitsoniii <2021pcecrmohit010@poornima.org>
2024-08-06 03:00:06 +0530

    Learning about Maths in JS

--- 01_Basics/06_nums_and_math.js
+++ 01_Basics/06_nums_and_math.js
@@ -28,3 +28,32 @@ Number
 // console.log(Number.MIN_SAFE_INTEGER);
 // console.log(Number.MAX_SAFE_INTEGER);
 // console.log(Number.EPSILON);
+
+
+
+// +++++++++++++++++++++++++   Maths   +++++++++++++++++++++++ //
+
+console.log(Math);
+
+// console.log(Math.abs(-5));
+// console.log(Math.abs(5));
+
+// console.log(Math.round(5.49));
+// console.log(Math.round(5.8));
+// console.log(Math.ceil(7.1));
+// console.log(Math.floor(7.9));
+
+
+// console.log(Math.min(4,6,52,78,62));
+// console.log(Math.max(4,6,52,78,62));
+
+
+console.log(Math.random());
+
+console.log(Math.random()*10);
+console.log(((Math.random())*10) + 1);    // Minimum value for this line will be 1.
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)  // it will always give the value between Max and Min.
