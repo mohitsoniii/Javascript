e83c7f7596c64c8b9776994a450a301dac4e737c
mohitsoniii <2021pcecrmohit010@poornima.org>
2024-08-06 02:43:59 +0530

    Learning about Numbers

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,30 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const num = 123.8555
+const num2 = 22.8555
+
+// console.log(num.toPrecision(2));
+// console.log(num2.toPrecision(1));
+// console.log(num2.toPrecision(2));
+// console.log(num2.toPrecision(3));
+// console.log(num2.toPrecision(4));
+
+const hundreads = 10000000
+// console.log(hundreads.toLocaleString('en-IN'));
+
+Number
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.EPSILON);
