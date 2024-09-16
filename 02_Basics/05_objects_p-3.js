6396a039fc2799a031c3d6208218053cf444e102
mohitsoniii <2021pcecrmohit010@poornima.org>
2024-09-17 05:24:21 +0530

    Learn about Destructing, JSON, API's

---
+++ 02_Basics/05_objects_p-3.js
@@ -0,0 +1,32 @@
+// ********** Destructuring  
+
+const course = {
+    name: "Js in Hindi",
+    price: 999,
+    courseInstructor : "Hitesh Choudary"
+}
+
+// course.courseInstructor 
+
+const {courseInstructor : instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+
+// ********** API's nad JSON *********//
+
+
+// this is a JSON
+// {
+//     "name" : "hitesh",
+//     "price" : "free",                 
+//     "courseName" : "JS in Hindi"
+// }
+
+[
+    {},
+    {},
+    {}
+]
