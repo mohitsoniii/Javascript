8bb818cda25119308f2ad2d7e8c8e583751fc81f
mohitsoniii <2021pcecrmohit010@poornima.org>
2024-09-17 04:17:22 +0530

    name updated

---
+++ 02_Basics/03_objects_p-1.js
@@ -0,0 +1,63 @@
+// ********** object constructors*********** // 
+// singleton
+
+// object.create
+
+
+
+// ************       Object Literals     ********** //
+
+const mySym = Symbol("Zodiac")
+
+const myInfo = {
+    name: "Mohit",
+    [mySym] : "Leo",
+    "Full Name" : "Mohit Soni",
+    age: 23,
+    location: "Jaipur",
+    email: "mohit@google.com",
+    isLoggedIn : true,
+    lastLoginDays : ["Saturday", "Sunday"]
+}
+// console.log(myInfo);
+
+
+// console.log(myInfo.email);
+// console.log(myInfo.lastLoginDays);
+// console.log(myInfo.Full Name);  // will give error
+// console.log(myInfo.mySym);      // will give error
+
+// console.log(myInfo["email"]);
+// console.log(myInfo["lastLoginDays"]);
+// console.log(myInfo["Full Name"]);
+// console.log(myInfo[mySym]);
+
+
+// Updating objects //
+
+// myInfo.email = "mohit@yt.com"
+// console.log(myInfo["email"]);
+
+// Object.freeze(myInfo)
+
+// myInfo.email = "mohit@microsoft.com"
+// console.log(myInfo["email"]);
+
+// console.log(myInfo);
+
+
+
+// creating function in an Object
+
+myInfo.greeting = function(){
+    console.log("Hello JS User!!");
+}
+// console.log(myInfo.greeting());
+
+
+myInfo.greetingTwo = function(){
+    console.log(`Hello JS User!!, ${this.name}`);
+}
+console.log(myInfo.greetingTwo());
+
+console.log(myInfo);
