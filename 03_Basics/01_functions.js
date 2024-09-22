538691dab16660b1fd2f48aa0b6e7f16d8729306
mohitsoniii <2021pcecrmohit010@poornima.org>
2024-09-23 02:54:57 +0530

    learnt functions with objects and arrays.

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -61,3 +61,71 @@ function loginUserMessage2(username = 'Mohan'){
 // console.log(loginUserMessage2('')); // will consider it as a empty string and ask to enter any argument
 
 // console.log(loginUserMessage2('Mohit'));
+
+
+
+
+
+
+
+
+
+
+//  ***************** FUNCTIONS WITH OBJECTS AND ARRAYS ****************** //
+
+
+
+
+
+
+
+function clacCartPrice(num1){
+    return num1
+}
+// console.log(clacCartPrice(2))   // output : 2
+
+
+function clacCartPrice1(...num1){     // (...) --> is known as Rest operator, it actually clubs the multiple values into one unit.
+    return num1
+}
+// console.log(clacCartPrice1(200,400,100,500))    // output :  [ 200, 400, 100, 500 ]
+
+
+function clacCartPrice2(val1, val2, ...num1){     // (...) --> is known as Rest operator, it actually clubs the multiple values into one unit.
+    return num1
+}
+// console.log(clacCartPrice2(200,400,100,500))   // output : [ 100, 500 ]   , here val1, val2 will be assigned as 200 and 400 respectivly and the rest will be clubbed by Rest op.
+
+
+
+// *****************    OBJECTS 
+
+const user = {
+    Username : 'Mohit',
+    Age : 23
+}
+
+function showData(anyObject){
+    console.log(`The user's name is ${anyObject.Username} and the user's age is ${anyObject.Age}`);
+}
+showData(user); //will run successfully, but we can also make it like given below.
+showData({
+    Username : 'Mohit',
+    Age : 23
+});
+
+
+
+
+
+
+//  *****************  ARRAYS 
+
+
+const itemPrices = [100,200,300,400,500]
+
+function showItemData(anyArray){
+    console.log(anyArray[2]);
+}
+showItemData(itemPrices); //will run successfully, but we can also make it like given below.
+showItemData([100,200,300,400,500]);
