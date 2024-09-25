a7a7b9f935add3e5dde15c56862a0b1bc08c4253
mohitsoniii <2021pcecrmohit010@poornima.org>
2024-09-25 18:10:19 +0530

    Learnt Switch Case statements.

---
+++ 04_Control_flow/02_switchcase.js
@@ -0,0 +1,45 @@
+// Switch Case is used in place of large number of Else if statements.
+
+// const month = 2
+
+// switch (month) {
+//     case 1:
+//         console.log("Month is Jan");
+//         break;
+//     case 2:
+//         console.log("Month is Feb");
+//         break;
+//     case 3:
+//         console.log("Month is March");
+//         break;
+//     case 4:
+//         console.log("Month is April");
+//         break;
+
+//     default:
+//         console.log("Default case match");
+//         break;
+// }
+
+
+
+const month = "feb"
+
+switch (month) {
+    case "jan":
+        console.log("Month is Jan");
+        break;
+    case "feb":
+        console.log("Month is Feb");
+        break;
+    case "march":
+        console.log("Month is March");
+        break;
+    case "april":
+        console.log("Month is April");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
