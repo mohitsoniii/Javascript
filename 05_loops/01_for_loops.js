c6c37d8c1ae3fc181f3fbc2e92740a212d62f41c
mohitsoniii <2021pcecrmohit010@poornima.org>
2024-10-01 03:24:40 +0530

    Updated

--- 05_loops/01_for_loops.js
+++ 05_loops/01_for_loops.js
@@ -5,6 +5,17 @@ for (let i = 0; i <= 10; i++) {
     // console.log(element);  
 }  // will print numbers from 0 to 10.
 
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log(`5 is the best number`);
+    }
+    // console.log(element);
+}
+
+
+//  >>>>>>>>>> Loopss in Arrays  <<<<<<<<<<<<<
+
 myArray = [2,4,6,2,9,0,5,43]
 
 for (let index = 0; index < myArray.length; index++) {
@@ -50,3 +61,29 @@ for (let i = 0; i < obj.length; i++) {
     console.log(element);
 }
 // console.log(obj.length);
+
+
+
+//  >>>>>>>>>>>>>>   Break And Continue   <<<<<<<<<<<<<
+
+// break
+
+for (let i = 1; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        console.log(`Detected number 5 :) `);
+        break                 // will break when 5 is occured.
+    }
+    console.log(`The value of i is ${element}`);
+}
+
+
+// Continue
+for (let i = 1; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        console.log(`Detected 5 :) `);
+        continue              // will skip when 5 is occured.
+    }
+    console.log(`The value of i is ${element}`);
+}
